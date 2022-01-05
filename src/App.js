a6f29b4d56ab7491364e1efdbc8067044c6d7a87
Isaac <uniqucode@gmail.com>
2022-01-05 02:37:57 +0100

    added visibility to cart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 import Cart from './components/Cart/Cart';
 
+
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
